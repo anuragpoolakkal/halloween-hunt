0f7b8b82e62ead6592e06b86f24a4e93ed0636fc
sainath378 <92171156+sai-na@users.noreply.github.com>
2022-10-28 17:13:33 +0530

    size

--- game.py
+++ game.py
@@ -1,9 +1,13 @@
 import pygame
 import os
 
+# shape parameters
+size = width, height = (1200, 800)
+
+
 pygame.init()
-win = pygame.display.set_mode((1200, 800))
-bg_img = pygame.image.load(os.path.join("images","level1_bg.png"))
+win = pygame.display.set_mode(size)
+bg_img = pygame.image.load(os.path.join("images", "level1_bg.png"))
 bg = pygame.transform.scale(bg_img, (1200, 800))
 
 width = 1200
@@ -15,9 +19,9 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
-    win.fill((0,0,0))
+    win.fill((0, 0, 0))
 
-    #Create looping background
+    # Create looping background
     win.blit(bg, (0, 0))
 
     pygame.display.update()
